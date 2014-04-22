93e295987521b8f9d31013e4d83ba929ff6a254d
Miroslav Popov <info@forexsb.com>
2014-04-22 13:03:43 +0300

    Added Demo App WinForms

---
+++ DemoAppWinForms/MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace DemoAppWinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.itmFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.itmGoHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmFile,
+            this.itmHelp});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(442, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // itmFile
+            // 
+            this.itmFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmOpen,
+            this.itmSave,
+            this.itmSaveAs});
+            this.itmFile.Name = "itmFile";
+            this.itmFile.Size = new System.Drawing.Size(37, 20);
+            this.itmFile.Text = "File";
+            // 
+            // itmOpen
+            // 
+            this.itmOpen.Name = "itmOpen";
+            this.itmOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.itmOpen.Size = new System.Drawing.Size(186, 22);
+            this.itmOpen.Text = "Open";
+            this.itmOpen.Click += new System.EventHandler(this.ItmOpen_Click);
+            // 
+            // itmSave
+            // 
+            this.itmSave.Name = "itmSave";
+            this.itmSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.itmSave.Size = new System.Drawing.Size(186, 22);
+            this.itmSave.Text = "Save";
+            this.itmSave.Click += new System.EventHandler(this.ItmSave_Click);
+            // 
+            // itmSaveAs
+            // 
+            this.itmSaveAs.Name = "itmSaveAs";
+            this.itmSaveAs.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.itmSaveAs.Size = new System.Drawing.Size(186, 22);
+            this.itmSaveAs.Text = "Save As";
+            this.itmSaveAs.Click += new System.EventHandler(this.ItmSaveAs_Click);
+            // 
+            // itmHelp
+            // 
+            this.itmHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmGoHelp});
+            this.itmHelp.Name = "itmHelp";
+            this.itmHelp.Size = new System.Drawing.Size(44, 20);
+            this.itmHelp.Text = "Help";
+            // 
+            // itmGoHelp
+            // 
+            this.itmGoHelp.Name = "itmGoHelp";
+            this.itmGoHelp.Size = new System.Drawing.Size(137, 22);
+            this.itmGoHelp.Text = "Online Help";
+            this.itmGoHelp.Click += new System.EventHandler(this.OnlineHelp_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 261);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(442, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Text = "Ready";
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(0, 24);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(442, 237);
+            this.textBox.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 283);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "ProgReporter Demo App WinForms";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem itmFile;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripMenuItem itmOpen;
+        private System.Windows.Forms.ToolStripMenuItem itmSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem itmSave;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ToolStripMenuItem itmHelp;
+        private System.Windows.Forms.ToolStripMenuItem itmGoHelp;
+    }
+}
+
---
+++ DemoAppWinForms/MainForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using ProgReporter;
+
+namespace DemoAppWinForms
+{
+    public partial class MainForm : Form
+    {
+        private string filePath = string.Empty;
+        private ProgStats stats;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            stats = new ProgStats();
+            stats.AppStart("cbc15a23946e34067c0085b2087ac33bf221a7d5");
+
+            // If your application uses licensing, you can set the license type of your app.
+            // Available license types are: Trial, Expired, Valid, NotValid, Unknown
+            stats.AppLicenseType = LicenseType.Valid;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Tell ProgReporter you are going to stop your application
+            stats.AppStop();
+        }
+
+        private void ItmOpen_Click(object sender, EventArgs e)
+        {
+            // Count "Open" usages as a feature with index 0
+            stats.FeatureClick(0);
+
+            filePath = GetOpenPath();
+            if (filePath == string.Empty)
+                return;
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            using (TextReader reader = new StreamReader(stream))
+                textBox.Text = reader.ReadToEnd();
+        }
+
+        private void ItmSave_Click(object sender, EventArgs e)
+        {
+            // Count "Save" usages as a feature with index 1
+            stats.FeatureClick(1);
+
+            if (filePath == string.Empty)
+            {
+                filePath = GetSaveAsPath();
+                if (filePath == string.Empty)
+                    return;
+            }
+
+            SaveTextToFile();
+        }
+
+        private void ItmSaveAs_Click(object sender, EventArgs e)
+        {
+            // Count "Save As" usages as a feature with index 2
+            stats.FeatureClick(2);
+
+            filePath = GetSaveAsPath();
+            if (filePath == string.Empty)
+                return;
+
+            SaveTextToFile();
+        }
+
+        private void OnlineHelp_Click(object sender, EventArgs e)
+        {
+            // Count "Help" usages as a feature with index 3
+            stats.FeatureClick(3);
+
+            try
+            {
+                Process.Start(@"http://progreporter.com/docs");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void SaveTextToFile()
+        {
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            using (TextWriter writer = new StreamWriter(stream))
+                writer.Write(textBox.Text);
+        }
+
+        private string GetSaveAsPath()
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                InitialDirectory = filePath,
+                FileName = Path.GetFileName(filePath),
+                AddExtension = true,
+                Title = "Save as...",
+                Filter = "Text file (*.txt)|*.txt"
+            };
+
+            return saveFileDialog.ShowDialog() == DialogResult.OK
+                ? saveFileDialog.FileName
+                : string.Empty;
+        }
+
+        private string GetOpenPath()
+        {
+            var saveFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = filePath,
+                FileName = Path.GetFileName(filePath),
+                AddExtension = true,
+                Title = "Open file...",
+                Filter = "Text file (*.txt)|*.txt"
+            };
+
+            return saveFileDialog.ShowDialog() == DialogResult.OK
+                ? saveFileDialog.FileName
+                : string.Empty;
+        }
+    }
+}
